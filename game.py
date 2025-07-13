d44d859f85eb15efd7845d4e2f1b778f3e35eeed
Allonaldo <j.g.m.rijpkema@student.utwente.nl>
2025-07-14 01:09:14 +0200

    Finished game logic

--- game.py
+++ game.py
@@ -3,7 +3,6 @@ from card import Deck, Player, Dealer
 
 def play():
 
-    # Setup game
     deck = Deck()
     deck.shuffle()
     dealer = Dealer()
@@ -17,36 +16,45 @@ def play():
         if not name:
             break
         players.append(Player(name))
+    # players.append[dealer]
 
     # Deal 2 cards to each player
-    for _ in range(2):
-        for player in players:
-            dealer.deal(player)
-
-    # TODO: Handle naturals before the play
-
-
-    # Blackjack loop
     for player in players:
-        
+        dealer.deal(deck, player)
+        dealer.deal(deck, player)
+
+    # Blackjack Loop
+
+    for player in players:
+
         score = player.check_hand()
+        if score == 21:
+            dealer_first_card = dealer.hand[0]
+            if dealer_first_card == 1 or dealer_first_card == 10:
+                if dealer.check_hand() == 21:
+                    dealer_natural = True
+                    # Return bet to player
+
+                else:
+                    dealer_natural = False
+
+        # Serve the players
         while 0 < score < 21:
-            # Ask to hit or stand
-            if player.choice():
-                dealer.deal(player)
-            else: 
+            if player.choice() == 0:
                 break
 
-    dealer_score = dealer.check_hand()
-    while dealer_score < 17:
-        dealer.deal(dealer)
-
-    
+        # Dealer's play
+        dealer_score = dealer.check_hand()
+        while 0 < dealer_score < 17:
+            dealer.hit() 
+                
+            
 
 
         
-    
-        
+
+                
+
 
         
     
